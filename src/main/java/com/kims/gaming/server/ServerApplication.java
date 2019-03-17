bf059cd2e4f68f2e131fabb246ce6b33fdfd9a77
devzucca <feel2174@gmail.com>
2019-03-17 19:06:49 +0100

    modified Reviewed Issues

--- src/main/java/com/kims/gaming/server/ServerApplication.java
+++ src/main/java/com/kims/gaming/server/ServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.kims.gaming.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -6,11 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
-public class DemoApplication {
+@EnableAutoConfiguration (exclude = {DataSourceAutoConfiguration.class})
+public class ServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(ServerApplication.class, args);
 	}
 
 }
