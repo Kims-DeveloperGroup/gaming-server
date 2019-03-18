07079c9dc96a1ed2ccccc244dc020b4844ada213
devzucca <feel2174@gmail.com>
2019-03-18 16:42:00 +0100

    Add .editconfig file

--- src/main/java/com/kims/gaming/server/ServerApplication.java
+++ src/main/java/com/kims/gaming/server/ServerApplication.java
@@ -9,7 +9,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @EnableAutoConfiguration (exclude = {DataSourceAutoConfiguration.class})
 public class ServerApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
 	}
 
