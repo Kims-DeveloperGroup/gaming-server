e3cc4ac20bbd1e13f4ac60706d4034290a76e42b
devzucca <feel2174@gmail.com>
2019-03-21 14:06:13 +0100

    Add Bean Annotation

--- src/main/java/com/kims/gaming/server/ServerApplication.java
+++ src/main/java/com/kims/gaming/server/ServerApplication.java
@@ -1,21 +1,14 @@
 package com.kims.gaming.server;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+
 import org.springframework.stereotype.Component;
 
-import java.io.IOException;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
 
 @Component
 @SpringBootApplication
