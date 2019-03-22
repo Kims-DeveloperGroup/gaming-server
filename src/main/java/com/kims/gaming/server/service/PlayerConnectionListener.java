0002e5035199eceda4cc13c7ebf27c61d3c87483
devzucca <feel2174@gmail.com>
2019-03-22 19:37:20 +0100

    Add Slf4j annotation

--- src/main/java/com/kims/gaming/server/service/PlayerConnectionListener.java
+++ src/main/java/com/kims/gaming/server/service/PlayerConnectionListener.java
@@ -1,30 +1,30 @@
 package com.kims.gaming.server.service;
 
-import jdk.nashorn.internal.runtime.logging.Logger;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
-@Logger
+@Slf4j
 @Service
 public class PlayerConnectionListener {
-    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(PlayerConnectionListener.class);
-    @Autowired
-    public void openConnection() throws IOException {
-        log.info("connecting");
+    @Bean
+    private void openConnection() throws IOException {
+        log.info("Starting....");
+        byte buffer[] = new byte[1024];
         DatagramSocket socket = new DatagramSocket(1234);
+        DatagramPacket packet = new DatagramPacket(buffer, 1024);
         boolean listen = true;
         while(listen) {
-            byte buffer[] = new byte[1024];
-            DatagramPacket packet = new DatagramPacket(buffer, 1024);
             socket.receive(packet);
             String msg = new String(packet.getData());
             System.out.println("Cleint : " + msg);
             String text = "connected";
             buffer = text.getBytes();
+            packet = new DatagramPacket(buffer, 1024);
             InetAddress address = packet.getAddress();
             int port = packet.getPort();
             packet = new DatagramPacket(buffer, buffer.length, address, port);
