11319f7b93d2efdd3c05f919db14a07055e5e209
feel2174 <37773812+feel2174@users.noreply.github.com>
2019-03-28 13:20:24 +0100

    Add PlayerConnectionListener to open connection from players (#3)
    * Build TestServer on Spring boot
    
    * modified Reviewed Issues
    
    * Modified not nessesary file& folder
    
    * Deleted dummy file
    
    * Modified description
    
    * Add .editconfig file
    
    * Add Bean framework(OOP)
    
    * Add Bean Annotation
    
    * Deleted not necessary spaces, Add Logger annotation
    
    * Rename class, modified method name
    
    * Add Slf4j annotation
    
    * Add json data connection
    
    * Add server stop method
    
    * Modified a part of Varible
    
    * Rename local var in camelCase

--- src/main/java/com/kims/gaming/server/ServerApplication.java
+++ src/main/java/com/kims/gaming/server/ServerApplication.java
@@ -1,16 +1,15 @@
 package com.kims.gaming.server;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-@EnableAutoConfiguration (exclude = {DataSourceAutoConfiguration.class})
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
 public class ServerApplication {
+    public static void main(String[] args)  { SpringApplication.run(ServerApplication.class, args);
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-	}
-
+    }
 }
---
+++ src/main/java/com/kims/gaming/server/service/PlayerConnectionListener.java
@@ -0,0 +1,39 @@
+package com.kims.gaming.server.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+@Slf4j
+@Service
+public class PlayerConnectionListener {
+    @Autowired
+    private void openConnection() throws IOException {
+        log.info("Starting....");
+        JSONObject json = new JSONObject();
+        DatagramSocket socket = new DatagramSocket(1234);
+        boolean listen = true;
+        while (listen) {
+            byte buffer[] = new byte[256];
+            DatagramPacket receiverPacket = new DatagramPacket(buffer, 256);
+            socket.receive(receiverPacket);
+            String msg = new String(receiverPacket.getData());
+            log.info("connected with Client..\nfrom. Client : " + msg);
+            json.put("status", "connnected");
+            byte buffer2[] = json.toJSONString().getBytes();
+            InetAddress address = receiverPacket.getAddress();
+            int port = receiverPacket.getPort();
+            DatagramPacket senderPacket = new DatagramPacket(buffer2, buffer2.length, address, port);
+            socket.send(senderPacket);
+            if (msg == "disconnected") {
+                log.info("server was disconnected");
+                socket.close();
+            }
+        }
+    }
+}
